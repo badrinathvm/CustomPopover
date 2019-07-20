5fd5c9aa51da541d1c05d3e55178df95a31beb40
badrinathvm@gmail.com <badrinathvm@gmail.com>
2019-07-19 21:19:16 -0700

    Added a test view to verify

--- CustomPopOver/ViewController.swift
+++ CustomPopOver/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import Popover
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let width = self.view.frame.width / 4
+        let aView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: width))
+        let options = [
+            .type(.up),
+            .cornerRadius(width / 2),
+            .animationIn(0.3),
+            .blackOverlayColor(UIColor.red),
+            .arrowSize(CGSize.zero)
+            ] as [PopoverOption]
+        let popover = Popover(options: options, showHandler: nil, dismissHandler: nil)
+        popover.show(aView, fromView: self.leftBottomButton)
     }
 
 
